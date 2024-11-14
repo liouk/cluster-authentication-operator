3f50ad6847ad7fa6e25535d65c24e3f3b12ff8b2
Ilias Rinis <irinis@redhat.com>
2024-11-14 14:13:41 +0100

    metadata: remove operands if external OIDC config is available

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -223,6 +223,7 @@ func prepareOauthOperator(
 		authOperatorInput.routeClient.RouteV1().Routes("openshift-authentication"),
 		authOperatorInput.configClient.ConfigV1().Authentications(),
 		authOperatorInput.authenticationOperatorClient,
+		authConfigChecker,
 		authOperatorInput.eventRecorder,
 	)
 
