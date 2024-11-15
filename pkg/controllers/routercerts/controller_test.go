5fe005fe20ba3e3c2bb37b85b2a90d8eaa4addcc
Ilias Rinis <irinis@redhat.com>
2024-11-15 08:32:10 +0100

    routercerts: remove operands if external OIDC config is available

--- pkg/controllers/routercerts/controller_test.go
+++ pkg/controllers/routercerts/controller_test.go
@@ -20,6 +20,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/mergepatch"
 	"k8s.io/client-go/kubernetes/fake"
+	corelistersv1 "k8s.io/client-go/listers/core/v1"
 	corev1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	clocktesting "k8s.io/utils/clock/testing"
@@ -27,6 +28,8 @@ import (
 	configv1 "github.com/openshift/api/config/v1"
 	operatorv1 "github.com/openshift/api/operator/v1"
 	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
+	operatorv1listers "github.com/openshift/client-go/operator/listers/operator/v1"
+	"github.com/openshift/cluster-authentication-operator/pkg/controllers/common"
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"github.com/openshift/library-go/pkg/operator/events"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
@@ -238,6 +241,15 @@ func TestValidateRouterCertificates(t *testing.T) {
 				secretsClient = fake.NewSimpleClientset()
 			}
 
+			authIndexer := cache.NewIndexer(func(_ interface{}) (string, error) {
+				return "cluster", nil
+			}, cache.Indexers{})
+			authIndexer.Add(&configv1.Authentication{
+				Spec: configv1.AuthenticationSpec{
+					Type: configv1.AuthenticationTypeIntegratedOAuth,
+				},
+			})
+
 			controller := routerCertsDomainValidationController{
 				operatorClient:    operatorClient,
 				ingressLister:     configv1listers.NewIngressLister(ingresses),
@@ -249,6 +261,11 @@ func TestValidateRouterCertificates(t *testing.T) {
 				routeName:         "test-route",
 				systemCertPool:    tc.systemCertPool,
 				secretsClient:     secretsClient.CoreV1(),
+				authConfigChecker: common.NewAuthConfigChecker(
+					&fakeInformer[configv1listers.AuthenticationLister]{configv1listers.NewAuthenticationLister(authIndexer)},
+					&fakeInformer[operatorv1listers.KubeAPIServerLister]{},
+					&fakeInformer[corelistersv1.ConfigMapLister]{},
+				),
 			}
 			err = controller.sync(context.TODO(), factory.NewSyncContext("testctx", events.NewInMemoryRecorder("test-recorder", clocktesting.NewFakePassiveClock(time.Now()))))
 			require.NoError(t, err)
@@ -460,4 +477,16 @@ func newServerCertificate(signer *cryptoMaterials, hosts ...string) *cryptoMater
 		panic(err)
 	}
 	return result
+}
+
+type fakeInformer[T any] struct {
+	lister T
+}
+
+func (f *fakeInformer[T]) Informer() cache.SharedIndexInformer {
+	return nil
+}
+
+func (f *fakeInformer[T]) Lister() T {
+	return f.lister
 }
