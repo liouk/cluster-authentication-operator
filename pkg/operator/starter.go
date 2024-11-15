e6acb56018a65c1a3b86bf58ceba83a01eaec688
Ilias Rinis <irinis@redhat.com>
2024-11-15 14:03:53 +0100

    payload: remove operands if external OIDC config is available

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -245,6 +245,7 @@ func prepareOauthOperator(
 		authOperatorInput.kubeClient.CoreV1(),
 		authOperatorInput.authenticationOperatorClient,
 		informerFactories.namespacedOpenshiftAuthenticationRoutes.Route().V1().Routes(),
+		authConfigChecker,
 		authOperatorInput.eventRecorder,
 	)
 
